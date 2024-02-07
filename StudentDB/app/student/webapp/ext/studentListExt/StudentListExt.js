c6c2f071a73e1831176697d95b82fac805ea9c1a
Harika-Bodapati <harika.bodapati2002@email.com>
2024-02-07 09:20:02 +0530

    changes

--- StudentDB/app/student/webapp/ext/studentListExt/StudentListExt.js
+++ StudentDB/app/student/webapp/ext/studentListExt/StudentListExt.js
@@ -4,9 +4,8 @@ sap.ui.define(["sap/m/MessageToast"], function (MessageToast) {
   return {
     SetAlumni: function (oBindingContext, aSelectedContexts) {
       aSelectedContexts.forEach((element) => {
-        MessageToast.show(element.sPath);
-        var oData = jQuery
-          .ajax({
+        //MessageToast.show(element.sPath);
+        var oData = jQuery.ajax({
             type: "PATCH",
             contentType: "application/json",
             url: "/odata/v4/student-db" + element.sPath,
@@ -15,11 +14,21 @@ sap.ui.define(["sap/m/MessageToast"], function (MessageToast) {
           .then(element.requestRefresh());
       });
     },
+    GetAlumni: async function(oBindingContext, aSelectedContexts) {
+      if(aSelectedContexts.length > 1) {
+        return false;
+      }
+      var oData = await jQuery.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: "/odata/v4/student-db" + aSelectedContexts[0].sPath,
+      })
+      return !oData.is_alumni;
+    },
     SetStudent: function (oBindingContext, aSelectedContext) {
       aSelectedContext.forEach((element) => {
         MessageToast.show(element.sPath);
-        var oData = jQuery
-          .ajax({
+        var oData = jQuery.ajax({
             type: "PATCH",
             contentType: "application/json",
             url: "/odata/v4/student-db" + element.sPath,
